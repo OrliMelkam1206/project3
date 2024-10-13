4e841c38ccda1fbb6d18e6a2128e9259aace631c
tamar-nisan <tamarnisan@hilms.tech>
2024-10-13 13:32:48 +0300

    add

--- js/signUp.js
+++ js/signUp.js
@@ -1,20 +1,14 @@
 function onRegister(event) {
     event.preventDefault()
-    let users = JSON.parse(localStorage.getItem("users"))
-
     let username = document.getElementById("username").value
     let phone = document.getElementById("phone").value
     let password = document.getElementById("password").value
     let email = document.getElementById("email").value
-    let userDataObj = { username, password, phone, email }
-
-    if (users === null) {
-        localStorage.setItem("users", JSON.stringify([userDataObj]))
-    }
-    else {
-        
-        localStorage.setItem("users", JSON.stringify([...users, userDataObj]))
-    }
+    let arrayContact=[];
+    let userDataObj = { username, password, phone, email, arrayContact }
+    
+    postUsers(userDataObj)
+   
     function backToLogin() {    
         document.getElementById("container").innerHTML = "";
         var signinContent = document.getElementsByTagName("template")[0].content.cloneNode(true);
