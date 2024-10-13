e008350eb7a3efb1a31a12a591e392b9006c85dc
orli-melkam <orlimelkam@hilma.tech>
2024-10-13 15:46:35 +0300

    add

--- js/contacts.js
+++ js/contacts.js
@@ -1,11 +1,11 @@
-const username = JSON.parse(localStorage.getItem("currentUser"));
-document.getElementById("userName").textContent += username.username;
-let main = document.getElementById("main");
-let contacts = username.arrayContact;
+const username2 = JSON.parse(localStorage.getItem("currentUser"));
+document.getElementsByClassName("userName")[0].textContent += username2.username;
+let main2 = document.getElementById("main");
+let contacts = username2.arrayContact;
 for(let i = 0; i < contacts.length; i++){
     let contact = document.createElement("div");
     contact.textContent = contacts.name;
     // contact.addEventListener("click", moveToSpecificContact());
-    main.appendChild(contact);
+    main2.appendChild(contact);
 }
 
