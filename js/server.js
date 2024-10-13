c1efbc0901b5abb542869dad7b408ac0fc83fd4c
tamar-nisan <tamarnisan@hilms.tech>
2024-10-13 15:45:32 +0300

    add

--- js/server.js
+++ js/server.js
@@ -25,46 +25,59 @@ function getContacts(){
 function postUsers(userDataObj){
 //פעולה שמקבלת אובייקט ומוסיפה אותו למערך המשתמשים
 let users = JSON.parse(localStorage.getItem("users"))
-
-
-// function postContactss(contactDataObj){
-//     //פעולה שמקבלת איש קשר ומוסיפה אותו למערך אנשי הקשר
-//     let contacts = JSON.parse(localStorage.getItem("cotacts"))
-//     if ( contacts === null) {
-//         localStorage.setItem("contacts", JSON.stringify([contactDataObj]))
-//     }
-//     else {
-        
-//         localStorage.setItem("contacts", JSON.stringify([...contacts, contactDataObj]))
-//     }
-//     }
-
 let exist=true;
-        if (userDataObj.username === "" || userDataObj.password === "") {
+   if (userDataObj.username === "" || userDataObj.password === "") {
            alert("הכנס שם משתמש או סיסמא") ;
             exist=false;
+    }
+    else
+     {
+      if (users != null)
+        for (let i = 0; i < users.length; i++) 
+        {
+             if (users[i].username=== userDataObj.username)
+              {
+               alert( "שם המשתמש תפוס ") ;
+               exist=false;
+               break;
+              }
+             else 
+              { 
+               exist = true 
+              }
+         }
+     }
+    if (exist) 
+     {
+      if (users === null) 
+        {
+          localStorage.setItem("users", JSON.stringify([userDataObj]))
         }
-         else {
-            if (users != null)
-            for (let i = 0; i < users.length; i++) {
-                if (users[i].username=== userDataObj.username) {
-                    alert( "שם המשתמש תפוס ") ;
-                    exist=false;
-                    break;
-                }
-                else { exist = true }
-            }
+      else 
+        {
+         localStorage.setItem("users", JSON.stringify([...users, userDataObj]))
         }
-        if (exist) {
-            if (users === null) {
-                localStorage.setItem("users", JSON.stringify([userDataObj]))
-            }
-            else {
-                
-                localStorage.setItem("users", JSON.stringify([...users, userDataObj]))
-            }
-            moveToContacts()
-            alert("ההרשמה בוצעה בהצלחה ");
-        }
+       moveToContacts()
+       alert("ההרשמה בוצעה בהצלחה ");
+      }
     
+    }
+
+    function postContact(contactDataObj,name){
+    //פעולה שמקבלת איש קשר ומוסיפה אותו למערך אנשי הקשר
+    let contacts = JSON.parse(localStorage.getItem("cotacts"))
+    if ( contacts === null) {
+        localStorage.setItem("contacts", JSON.stringify([contactDataObj]))
+    }
+    else {
+        
+        localStorage.setItem("contacts", JSON.stringify([...contacts, contactDataObj]))
+    }
+    
+    let users = JSON.parse(localStorage.getItem("users"))
+    for(let i=0; i<users.length; i++){
+      if(users[i].username===name){
+        users[i].arrayContact.push(contactDataObj)
+      }
+    }
     }
